ed469f75fef4747360d901a2fd4cdc1bf8ac9b21
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-03-13 03:04:16 +0700

    [feature][comp/button] update test

--- src/components/button/__test__/button.test.js
+++ src/components/button/__test__/button.test.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Button from '..';
 
-test('Button Test Render', () => {
-  const { container } = render(
-    <Button variant="primary" block>
-      Button
-    </Button>,
+const handleClick = (e) => {
+  return e;
+};
+
+test('Button Render Test', () => {
+  const { container } = render(<Button>Button</Button>);
+  expect(container).toMatchSnapshot();
+});
+
+test('Button Event Test', () => {
+  const { container, getByText } = render(
+    <Button onClick={handleClick}>Click</Button>,
   );
+
+  fireEvent.click(getByText('Click'));
+  fireEvent.doubleClick(getByText('Click'));
+
+  expect(container).toMatchSnapshot();
+});
+
+test('Button Variants', () => {
+  const { container } = render(
+    <>
+      <Button variant="primary">Button</Button>
+      <Button variant="secondary">Button</Button>
+      <Button variant="success">Button</Button>
+      <Button variant="warning">Button</Button>
+      <Button variant="danger">Button</Button>
+      <Button variant="neutral">Button</Button>
+      <Button variant="clean">Button</Button>
+    </>,
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test('Button Block', () => {
+  const { container } = render(<Button block>Button</Button>);
   expect(container).toMatchSnapshot();
 });
 
@@ -20,16 +51,16 @@ test('Button Disabled', () => {
 test('Button Size', () => {
   const { container } = render(
     <>
-      <Button size="small">Small</Button>
-      <Button>Default</Button>
-      <Button size="large">Large</Button>
+      <Button size="small">Button</Button>
+      <Button>Button</Button>
+      <Button size="large">Button</Button>
     </>,
   );
   expect(container).toMatchSnapshot();
 });
 
 test('Button Outline Style', () => {
-  const { container } = render(<Button outline>Outline</Button>);
+  const { container } = render(<Button outline>Button</Button>);
   expect(container).toMatchSnapshot();
 });
 
