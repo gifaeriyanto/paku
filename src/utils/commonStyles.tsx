72c3093c5c07d01c2a67a4ff7d5118c3d446d9fe
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-03-31 20:07:00 +0700

    [improvements][utils/commonStyle] update isOutline

--- src/utils/commonStyles.tsx
+++ src/utils/commonStyles.tsx
@@ -4,10 +4,25 @@ import { TVariants } from './types';
 export const isOutline = (
   variant: TVariants,
   outline?: boolean | 'withColor',
+  withoutEvent?: boolean,
 ) => {
   let borderColor = Color.border;
   if (outline === 'withColor') {
     borderColor = Color[variant].main;
+  }
+
+  let events = {};
+  if (!withoutEvent) {
+    events = {
+      '&:hover': {
+        color: Color[variant].hover,
+        border: `1px solid ${Color[variant].hover}`,
+      },
+      '&:active': {
+        color: Color[variant].active,
+        border: `1px solid ${Color[variant].active}`,
+      },
+    };
   }
 
   if (outline) {
@@ -15,16 +30,7 @@ export const isOutline = (
       backgroundColor: '#fff',
       border: `1px solid ${borderColor}`,
       color: Color[variant].main,
-      '&:hover': {
-        backgroundColor: '#fff',
-        color: Color[variant].hover,
-        border: `1px solid ${Color[variant].hover}`,
-      },
-      '&:active': {
-        backgroundColor: '#fff',
-        color: Color[variant].active,
-        border: `1px solid ${Color[variant].active}`,
-      },
+      ...events,
     };
   }
   return {};
