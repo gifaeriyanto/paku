4fc55d8e354f2121451f4f28dc45f10a314912c0
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-04-14 22:59:52 +0700

    register all components to components/index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,1 @@
-export { default as Badge } from './badge';
 export { default as Button } from './button';
-export { default as Checkbox } from './checkbox';
-export { Container, Row, Col } from './grid';
-export { default as Select } from './select';
-export { default as Tabs } from './tabs';
